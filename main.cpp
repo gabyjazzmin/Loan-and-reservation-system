008b5c6f3cd37ba19a4820b4bb54e3b48445a4cf
Gabriela Álvarez <61790728+gabyjazzmin@users.noreply.github.com>
2020-06-05 22:36:36 -0500

    Errores en el 3, 4 y 5
    Se despliega mal los valores del idCliente

--- main.cpp
+++ main.cpp
@@ -43,7 +43,7 @@ int main()
     ifstream archReserva;
     int dia, mes, anio, idM, cID;
     Reserva reservar;
-    Fecha fdate, defDate;
+    Fecha fdate, defdate;
     Libro libro;
     ofstream finReservar;
 
@@ -63,13 +63,14 @@ int main()
   bool valorEncontrado = false;
   int iV = 0;
   string titulo;
-  int dia1, mes2, anio2;
+  int dia2, mes2, anio2;
   int verf = 0;
   char claseMaterial;
   bool fechaOcupada = false;
-  bool tieneReservacion = false; //Cambiar nombre
+  bool tieneReservacion = false; 
   int numMaterial=0;
   int idMaux=0;
+  Fecha endRes;
   
 
     
@@ -129,37 +130,47 @@ int main()
       reservasIniciales++;
     }
   } archReserva.close();
+
+  //----------------------------------------------------------
+
   while(opcion == true){
+    cout << "-|-|-|-|-|-|-|-|-|-|-|-|-|-|"<< endl;
     cout << "Bienvenido al menú de opciones del sistema de reserva" << endl;
     cout << "-|-|-|-|-|-|-|-|-|-|-|-|-|-|" << endl;
-		cout << " 1- Display the list of Materials " << endl;
-		cout << " 2- Display the list of Reservation " << endl;
-		cout << " 3- Display the reservation of a Material " << endl;
-		cout << " 4- Display the reservation of a Date" << endl;
-		cout << " 5- Do a reservation " << endl;
-		cout << " 6- End program " << endl;
+		cout << " 1- Mostrar lista de materiales" << endl;
+		cout << " 2- Mostrar lista de reservaciones" << endl;
+		cout << " 3- Mostrar la reservación de un material en especifico. " << endl;
+		cout << " 4- Mostrar la reservación de una fecha en especifico." << endl;
+		cout << " 5- Hacer una reservación. " << endl;
+		cout << " 6- Finalizar el programa. " << endl;
 		cin >> menu;
     
     switch(menu){
+//------------------------------------------------------------
       case 1:
       for(int x = 0; x<numMaterial; x++){
         arrDataMaterial[x]->muestraDatos();
       }
+
+//------------------------------------------------------------      
       case 2:
       for(int x = 0; x<numReservas; x++){
         Fecha aux=arrDataReservacion[x].getfechaReservacion();
         mat = arrDataReservacion[x].getidMaterial();
         cout<<mat<<endl;
+      //En caso de que el material esté entre 100 y 200
         if(mat>=100 && mat<200){
           dias = 7;
           Fecha tFecha = arrDataReservacion[x].setcalculaFechaFinReserva(dias);
           for(int y=0; y<numMaterial; y++){
             if(arrDataReservacion[x].getidMaterial()==arrDataMaterial[y]->getidMaterial()){
-              cout<<arrDataMaterial[y]->gettitulo()<<" "<<aux<<" "<<tFecha;
+              cout<<arrDataMaterial[y]->gettitulo()<< " Fecha Inicial"<<" "<< aux << " Fecha Final " <<" "<<tFecha << endl;
+              cout << " -----------------------------" << endl;
             }
           }
 
         }
+        //En caso de que el material esté entre 200 y 300
         if(mat>=200 && mat<300){
           dias = 2;
           Fecha tFecha = arrDataReservacion[x].setcalculaFechaFinReserva(dias);
@@ -167,17 +178,21 @@ int main()
             //cout<<y<<endl;
             //cout<<arrDataReservacion[x].getidMaterial()<<"   "<<arrDataMaterial[y]->getidMaterial()<<endl;
             if(arrDataReservacion[x].getidMaterial()==arrDataMaterial[y]->getidMaterial()){
-              cout<<arrDataMaterial[y]->gettitulo()<<" "<<aux<<" "<<tFecha;
+              cout<<arrDataMaterial[y]->gettitulo()<< " Fecha Inicial"<<" "<< aux << " Fecha Final " <<" "<<tFecha << endl;
+              cout << " -----------------------------" << endl;
             }
           }
 
         }
+
+        //En caso de que el material sea mayor a 300
         if(mat>=300){
           dias = 1;
           Fecha tFecha = arrDataReservacion[x].setcalculaFechaFinReserva(dias);
           for(int y=0; y<numMaterial; y++){
             if(arrDataReservacion[x].getidMaterial()==arrDataMaterial[y]->getidMaterial()){
-              cout<<arrDataMaterial[y]->gettitulo()<<" "<<aux<<" "<<tFecha;
+              cout<<arrDataMaterial[y]->gettitulo()<< " Fecha Inicial"<<" "<< aux << " Fecha Final " <<" "<<tFecha << endl;
+              cout << " -----------------------------" << endl;
             }
           }
 
@@ -186,31 +201,140 @@ int main()
 
       }
 
+      break;
+
+//------------------------------------------------------------
       case 3:
+      cout << "Introduce el ID del material" << endl;
+      cin >> buscaId;
+      valorEncontrado = false;
 
+      for(int il = 0; il < 50; il++){
+        if(arrDataReservacion[il].getidMaterial() == buscaId){
+          arrDataReservacion[il].getfechaReservacion();
+          for(iV = 0; iV <20; iV++){
+            if(arrDataReservacion[il].getidMaterial() == arrDataMaterial[iV]->getidMaterial()){
+              titulo = arrDataMaterial[iV]->gettitulo();
+              cout << "El titulo del material es: " << titulo << endl;
+								 if (arrDataReservacion[il].getidMaterial() >= 100 && arrDataReservacion[il].getidMaterial() < 200) {
+
+									 
+									 fdate= arrDataReservacion[il].setcalculaFechaFinReserva(10);
+									 cout << "Deadline: ";
+									 fdate.muestraDatos();
+									 valorEncontrado = true;
+								 }
+								 else if(arrDataReservacion[il].getidMaterial() >= 200 && arrDataReservacion[il].getidMaterial() < 300) {
+
+
+									 fdate = arrDataReservacion[il].setcalculaFechaFinReserva(3);
+									 cout << "Deadline: ";
+									 fdate.muestraDatos();
+									 valorEncontrado = true;
+								 }
+								 else if (arrDataReservacion[il].getidMaterial() >= 300 && arrDataReservacion[il].getidMaterial() < 400) {
+
+
+									 fdate = arrDataReservacion[il].setcalculaFechaFinReserva(1);
+									 cout << "Deadline: ";
+									 fdate.muestraDatos();
+									 valorEncontrado = true;
+								 }
+								 
+							 }
+							
+
+						}
+						
+					}
+						
+				}
+
+				if (valorEncontrado == false) {
+					cout << "ID no encontrado " << endl << endl;
+				}
+
+
+    break;
+//------------------------------------------------------------
       case 4:
+				cout << "-Proceso de fecha-" << endl;
+				cout << "Introduce el dia:  ";
+				cin >> dia2;
+				cout <<endl << "Introduce el mes: ";
+				cin >> mes2;
+				cout << endl << "Introduce el año: ";
+				cin >> anio2;
+				cout << endl;
+				defdate.setdd(dia2);
+				defdate.setmm(mes2);
+				defdate.setaa(anio2);
+				
+				for (int i = 0; i < 50; i++) {
+					
+					if (arrDataReservacion[i].getidMaterial() > 100 && arrDataReservacion[i].getidMaterial() < 200) {
+						dias = 10;
+					}
+					else if (arrDataReservacion[i].getidMaterial() > 200 && arrDataReservacion[i].getidMaterial() < 300) {
+						dias = 3;
+					}
+					else if (arrDataReservacion[i].getidMaterial() > 300 && arrDataReservacion[i].getidMaterial() < 400) {
+						dias = 1;
+					}
 
+					endRes = arrDataReservacion[i].getReservacion2() + dias;
+					
+					 
+					if (defdate >= arrDataReservacion[i].getReservacion2() && defdate <= endRes) {
+						for (int j = 0; j < 20; j++) {
+							
+							if (arrDataMaterial[j]->getidMaterial() == arrDataReservacion[i].getidMaterial()) {
+								cout << "Fecha inicial: ";
+								arrDataReservacion[i].getReservacion2().muestraDatos();
+								cout << "Titulo: " << arrDataMaterial[j]->gettitulo() << endl;
+								cout << "Id cliente: " << arrDataReservacion[i].getidCliente() << endl;
+								cout <<"Deadline: "<< endRes << endl;
+								cout << endl;
+							
+
+							}
+							
+						}
+						
+						
+						
+					}
+				}
+					
+
+
+    break;
+//------------------------------------------------------------
       case 5:
-/*
-{
+      {
+
+
       fechaOcupada = false;
       tieneReservacion = false;
 
-      	cout << "Let's do a reservation!" << endl;
-				cout << "Introduce your client ID: " << endl;
+      	cout << "-Proceso de hacer una reservación-" << endl;
+				cout << "Introduce tu ID de cliente : " << endl;
 				cin >> cID;
-				cout << "Introduce the Material ID:" << endl;
-				cin >> idMaterial;
-				cout << "Introduce the Date of Reservation " << endl;
-				cout << "Day:" << endl;
+        cout << cID;
+				cout << "Introduce tu ID del material: " << endl;
+				cin >> materialId;
+
+        
+				cout << "-Proceso de fecha del material-" << endl;
+				cout << "Día:" << endl;
 				cin >> dia;
-				defDate.setdd(dia);
-				cout << "Month: " << endl;
+				defdate.setdd(dia);
+				cout << "Mes: " << endl;
 				cin >> mes;
-				defDate.setmm(mes);
-				cout << "Year:" << endl;
+				defdate.setmm(mes);
+				cout << "Año:" << endl;
 				cin >> anio;
-				defDate.setaa(anio);
+				defdate.setaa(anio);
 
 for (int l = 0; l < 20; l++) { //ENTRA EN UN CICLO POR TODOS LOS MATERIALES PARA CONSEGUIR EL NUMERO DE DIAS
 
@@ -226,10 +350,43 @@ for (int l = 0; l < 20; l++) { //ENTRA EN UN CICLO POR TODOS LOS MATERIALES PARA
 							dias = 1;
 						}
 					}
-					else {}
-          
-}
-*/
+					else {};
+        }
+      
+    for (int i = 0; i < 50; i++) { //ENTRA EN UN CICLO POR TODAS LAS RESERVACIONES
+
+					if (arrDataReservacion[i].getidMaterial() == idMaterial) { //SI EL MATERIAL ID DE LA RESERVACION COINCIDE
+						tieneReservacion = true;
+						if ((arrDataReservacion[i].getReservacion2() >= defdate) && (arrDataReservacion[i].setcalculaFechaFinReserva(dias)<= defdate)) {
+							cout << "Ya está reservado. " << endl;
+							fechaOcupada = true;
+
+						}
+						else {
+						}
+					}
+				}
+if (tieneReservacion && !fechaOcupada)
+					{
+						cout << "Reservado Correctamente"<<endl;
+            cout << "A continuación regresarás al menú" << endl;
+						Reserva finalReserve(idMaterial, cID, defdate);
+						arrDataReservacion[numReservas] = finalReserve;
+						numReservas++;
+					}
+					else {};	
+				
+				if (!tieneReservacion)
+				{
+					cout << "Reservado Correctamente"<<endl;
+					Reserva finalReserve(idMaterial, cID, defdate);
+					arrDataReservacion[numReservas] = finalReserve;
+					numReservas++;
+				}
+				
+				break;
+    }
+//------------------------------------------------------------
       case 6:
       opcion = false;
       break;
